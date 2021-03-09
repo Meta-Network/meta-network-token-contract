0d6c579355be962acd32ebb74cec706dcddb980a
Frank Wei <frank@frankwei.xyz>
2021-03-09 19:22:49 +0800

    feat: add permit to META

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -3,14 +3,14 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20WithPermit.sol";
 
-contract MetaToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("META Token", "META") {
+contract MetaToken is ERC20WithPermit, Ownable {
+    constructor(uint256 initialSupply) ERC20WithPermit("META Token", "META") {
         _mint(msg.sender, initialSupply);
     }
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
